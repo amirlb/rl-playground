59968d15d803ee0c8043211617459cc84f7a5726
Amir Livne Bar-on <amir.livne.baron@gmail.com>
2025-05-01 22:22:20 +0300

    tic tac toe naive rl

--- base.py
+++ base.py
@@ -1,47 +1,55 @@
+from dataclasses import dataclass
 from math import inf
 import random
+from typing import TypeVar, Protocol
+
+Board = TypeVar("Board")
+Move = TypeVar("Move")
 
 
-class Game:
-    def starting_position(self):
+class Game(Protocol):
+    def starting_position(self) -> Board:
         raise NotImplementedError
 
-    def legal_moves(self, board):
+    def legal_moves(self, board: Board) -> tuple[list[Move], float | None]:
         """List of legal moves, and game value if terminal"""
         raise NotImplementedError
 
-    def play_move(self, board, move):
+    def play_move(self, board: Board, move: Move) -> Board:
         raise NotImplementedError
 
 
 class Player:
-    def choose_move(self, board):
+    def choose_move(self, board: Board) -> Move:
         raise NotImplementedError
 
 
 class RandomPlayer(Player):
-    def __init__(self, game):
+    def __init__(self, game: Game):
         self.game = game
 
-    def choose_move(self, board):
+    def choose_move(self, board: Board) -> Move:
         moves, _ = self.game.legal_moves(board)
         return random.choice(moves)
 
 
 class ValueEstimator:
-    def value(self, board):
+    def value(self, board: Board) -> float:
         """Float between -1 and 1"""
         raise NotImplementedError
 
 
 class ValuePlayer(Player):
-    def __init__(self, game, value_estimator: ValueEstimator):
+    def __init__(self, game: Game, value_estimator: ValueEstimator, exploration_rate: float = 0.0):
         self.game = game
         self.value_estimator = value_estimator
+        self.exploration_rate = exploration_rate
 
-    def choose_move(self, board):
+    def choose_move(self, board: Board) -> Move:
         moves, value = [], -inf
         legal_moves, _ = self.game.legal_moves(board)
+        if self.exploration_rate > 0 and random.random() < self.exploration_rate:
+            return random.choice(legal_moves)
         for move in legal_moves:
             v = -self.value_estimator.value(self.game.play_move(board, move))
             if v > value:
@@ -55,42 +63,59 @@ class ValuePlayer(Player):
 class OptimalValueEstimator(ValueEstimator):
     """Only use for games with a small number of states"""
 
-    VALUES = {}
-
     def __init__(self, game):
         self.game = game
+        self.values = {}
+        self.compute_value(game.starting_position())
 
-    def value(self, board):
-        if board not in self.VALUES:
-            self.VALUES[board] = self.compute_value(board)
-        return self.VALUES[board]
+    def value(self, board: Board) -> float:
+        return self.values[board]
 
-    def compute_value(self, board):
+    def compute_value(self, board: Board) -> None:
+        if board in self.values:
+            return
         legal_moves, value = self.game.legal_moves(board)
         if value is not None:
-            return value
-        return max(-self.value(self.game.play_move(board, move)) for move in legal_moves)
+            self.values[board] = value
+            return
+        for move in legal_moves:
+            self.compute_value(self.game.play_move(board, move))
+        self.values[board] = max(-self.values[self.game.play_move(board, move)] for move in legal_moves)
 
 
-def simulate_game(game, player1, player2):
+class GameTreeIndexer(dict):
+    def __init__(self, game: Game):
+        super().__init__({board: i for i, board in enumerate(OptimalValueEstimator(game).values.keys())})
+
+
+@dataclass
+class Episode:
+    decisions: list[tuple[Board, Move]]
+    last_position: Board
+    value: float
+
+
+def simulate_game(game: Game, player1: Player, player2: Player) -> Episode:
     board = game.starting_position()
-    positions = []
+    decisions = []
     player_value = 1
     while True:
-        positions.append(board)
         _, value = game.legal_moves(board)
         if value is not None:
-            return (positions, value * player_value)
-        board = game.play_move(board, player1.choose_move(board))
+            return Episode(decisions, board, value * player_value)
+        move = player1.choose_move(board)
+        decisions.append((board, move))
+        board = game.play_move(board, move)
         player1, player2, player_value = player2, player1, -player_value
 
 
-def gather_stats(game, player1, player2, n_games):
+def gather_stats(game: Game, player1: Player, player2: Player, n_games: int) -> list[int]:
     results = [0, 0, 0]
-    player_value = 1
     for _ in range(n_games):
         if random.random() < 0.5:
-            player1, player2, player_value = player2, player1, -player_value
-        _, result = simulate_game(game, player1, player2)
-        results[1 - result * player_value] += 1
+            episode = simulate_game(game, player1, player2)
+            results[1 + episode.value] += 1
+        else:
+            episode = simulate_game(game, player2, player1)
+            results[1 - episode.value] += 1
     return results
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,20 +1,26 @@
-from base import Game, OptimalValueEstimator, RandomPlayer, ValuePlayer, gather_stats, simulate_game
+import random
+import torch
+from base import Episode, Game, GameTreeIndexer, OptimalValueEstimator, Player, RandomPlayer, ValueEstimator, ValuePlayer, gather_stats, simulate_game
+
+
+TTTPosition = tuple[int, int]
+TTTMove = int
 
 
 class TicTacToeGame(Game):
     LINES = [0b000000111, 0b000111000, 0b111000000, 0b001001001, 0b010010010, 0b100100100, 0b100010001, 0b001010100]
 
     @classmethod
-    def starting_position(cls):
+    def starting_position(cls) -> TTTPosition:
         return (0, 0)
 
     @classmethod
-    def fmt(cls, board):
+    def fmt(cls, board: TTTPosition) -> str:
         my, other = board
         return " ".join("".join("X" if my & (1 << i) else "O" if other & (1 << i) else "." for i in range(j, j+3)) for j in range(0, 9, 3))
 
     @classmethod
-    def legal_moves(cls, board):
+    def legal_moves(cls, board: TTTPosition) -> tuple[list[TTTMove], float | None]:
         my, other = board
         whole = my | other
         if whole == 0b111111111:
@@ -27,23 +33,103 @@ class TicTacToeGame(Game):
         return [i for i in range(9) if whole & (1 << i) == 0], None
 
     @classmethod
-    def play_move(cls, board, idx):
+    def play_move(cls, board: TTTPosition, move: TTTMove) -> TTTPosition:
         my, other = board
         whole = my | other
-        if whole & (1 << idx):
-            raise ValueError(f"Cell {idx} is not empty")
-        return (other, my | (1 << idx))
+        if whole & (1 << move):
+            raise ValueError(f"Cell {move} is not empty")
+        return (other, my | (1 << move))
+
+
+class VectorValueEstimator(ValueEstimator):
+    def __init__(self):
+        self.index = GameTreeIndexer(TicTacToeGame)
+        self.values = [0] * len(self.index)
+        # opt = OptimalValueEstimator(TicTacToeGame)
+        # for board, ind in self.index.items():
+        #     self.values[ind] = opt.value(board)
+
+    def value(self, board: TTTPosition) -> float:
+        return self.values[self.index[board]]
+
+
+class PytorchVectorValueEstimator(ValueEstimator):
+    def __init__(self):
+        self.index = GameTreeIndexer(TicTacToeGame)
+        self.values = torch.zeros(len(self.index))
+
+    def value(self, board: TTTPosition) -> float:
+        return self.values[self.index[board]]
+
+    def update(self, board: TTTPosition, value: float) -> None:
+        self.values[self.index[board]] = value
+
+
+class ValueOptimizer:
+    def train(self, episodes: list[Episode], estimator: ValueEstimator) -> None:
+        raise NotImplementedError
+
+
+def train_self_play(game: Game, n_iters: int, n_games_per_iter: int, estimator: ValueEstimator, exploration_rate: float, optimizer: ValueOptimizer) -> None:
+    evaluate_player(ValuePlayer(game, estimator), "Before training")
+    explorer = ValuePlayer(game, estimator, exploration_rate)
+    for i in range(n_iters):
+        episodes = [simulate_game(game, explorer, explorer) for _ in range(n_games_per_iter)]
+        optimizer.train(episodes, estimator)
+        evaluate_player(ValuePlayer(game, estimator), f"After {i + 1} iterations")
+
+
+class OptimizerNaive1(ValueOptimizer):
+    def __init__(self, max_diff: float):
+        self.max_diff = max_diff
+
+    def train(self, episodes: list[Episode], estimator: VectorValueEstimator) -> None:
+        for ep in episodes:
+            for i, (board, _) in enumerate(ep.decisions):
+                self.update_towards(estimator, board, ep.value * (-1)**i)
+            self.update_towards(estimator, ep.last_position, ep.value * (-1)**len(ep.decisions))
+
+    def update_towards(self, estimator: VectorValueEstimator, board: TTTPosition, new_value: float) -> None:
+        i = estimator.index[board]
+        d = new_value - estimator.values[i]
+        if d > self.max_diff:
+            d = self.max_diff
+        elif d < -self.max_diff:
+            d = -self.max_diff
+        estimator.values[i] += d
+        assert -1 <= estimator.values[i] <= 1
+
+
+class OptimizerNaive2(ValueOptimizer):
+    def __init__(self, m: float):
+        self.m = m
+
+    def train(self, episodes: list[Episode], estimator: VectorValueEstimator) -> None:
+        for ep in episodes:
+            for i, (board, _) in enumerate(ep.decisions):
+                self.update_towards(estimator, board, ep.value * (-1)**i)
+            self.update_towards(estimator, ep.last_position, ep.value * (-1)**len(ep.decisions))
+
+    def update_towards(self, estimator: VectorValueEstimator, board: TTTPosition, new_value: float) -> None:
+        i = estimator.index[board]
+        estimator.values[i] = (1 - self.m) * estimator.values[i] + self.m * new_value
+        assert -1 <= estimator.values[i] <= 1
+
+
+def evaluate_player(player: Player, name: str, n_games: int = 1000) -> float:
+    loss, draw, win = gather_stats(TicTacToeGame, player, RandomPlayer(TicTacToeGame), n_games)
+    print(f"Out of {n_games} games, {name}: wins: {win/n_games:5.1%}, draws: {draw/n_games:5.1%}, loss: {loss/n_games:5.1%}")
 
 
 def main():
-    n_games = 1000
-    random_player = RandomPlayer(TicTacToeGame)
-    optimal_player = ValuePlayer(TicTacToeGame, OptimalValueEstimator(TicTacToeGame))
-    win, draw, loss = gather_stats(TicTacToeGame, optimal_player, random_player, n_games)
-    print(f"Out of {n_games} games:")
-    print(f"Optimal wins: {win} ({win/n_games*100:.1f}%)")
-    print(f"Random wins: {loss} ({loss/n_games*100:.1f}%)")
-    print(f"Draws: {draw} ({draw/n_games*100:.1f}%)") 
+    random.seed(123)
+    evaluate_player(RandomPlayer(TicTacToeGame), "Random")
+    evaluate_player(ValuePlayer(TicTacToeGame, OptimalValueEstimator(TicTacToeGame)), "Optimal")
+    estimator = VectorValueEstimator()
+    # train_self_play(TicTacToeGame, n_iters=500, n_games_per_iter=10, exploration_rate=0.2, optimizer=OptimizerNaive1(max_diff=0.1), estimator=estimator)
+    # train_self_play(TicTacToeGame, n_iters=500, n_games_per_iter=10, exploration_rate=0.2, optimizer=OptimizerNaive2(m=0.1), estimator=estimator)
+    train_self_play(TicTacToeGame, n_iters=500, n_games_per_iter=10, exploration_rate=0.2, optimizer=OptimizerNaive2(m=0.1), estimator=estimator)
+    evaluate_player(ValuePlayer(TicTacToeGame, estimator), "After training")
 
 
 if __name__ == '__main__':
